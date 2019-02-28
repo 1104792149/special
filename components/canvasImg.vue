2262f4b42f8496ea8fb1a04652881b80b838bf67
lzj <1104792149@qq.com>
2019-03-01 00:06:16 +0800

    海报

--- components/canvasImg.vue
+++ components/canvasImg.vue
@@ -1,19 +1,142 @@
 <template>
-	<view>
-		<canvas canvas-id="myCanvas"  style="border: 1px solid;width: 300px;height: 300px;"/> 
+	<view class="canvas">
+		<canvas canvas-id="myCanvas"   :style="'width:'+imgValue.width+'px;height:'+imgValue.height+'px;border: 1px solid orangered;'"/> 
+		<button type="primary"  @click="canvasTap()">开始</button>
 	</view>
 </template>
 
 <script>
 	export default {
+		props:['imgValue'],
 		data() {
 			return {
-				
+				ctx:''
 			};
+		},
+		computed:{
+			
+		},
+		methods:{
+			canvasTap(){
+				 this.ctx = uni.createCanvasContext('myCanvas');
+				 this.ctx.beginPath()
+				this.imgValue.views.forEach((item,index)=>{
+					switch (item.type){
+						//矩形 radius
+						case 'rect':
+							this.canvasBugTap(item)
+						break;
+						//图片
+						case 'image':	
+							this.canvasImgTap(item)
+						break;
+						//圆形图片
+						case 'radius':	
+							this.canvasRyTap(item)
+						break;
+					};
+				});
+				//告诉 <canvas/> 组件你要将刚刚的描述绘制上去：
+				this.ctx.draw(false);
+			},
+			//矩形
+			canvasBugTap(value){
+				console.info(value)
+				//设置绘图上下文的填充色为红色：
+				this.ctx.setFillStyle(value.background)
+				//用 fillRect(x, y, width, height) 方法画一个矩形，填充为刚刚设置的红色：
+				this.ctx.fillRect(value.x, value.y, value.width, value.height);
+			},
+			//画图片
+			canvasImgTap(value){
+				//drawImage(sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+				//dx, dy, dWidth, dHeight ，裁剪的区域和坐标可要可不要
+				//图片路径，x轴坐标 y轴坐标 长 高
+				this.ctx.drawImage(value.url, value.x, value.y, value.width, value.height)
+			},
+			//画圆形图片
+			canvasRyTap(value){
+				
+				let left=80;
+				let top=50;
+				let height=50;
+				let width=50;
+				let borderRadius=width/2;
+				//保存当前画布区域
+				this.ctx.save()
+				//裁剪 可以在使用 clip() 方法前通过使用 save() 方法对当前画布区域进行保存，并在以后的任意时间对其进行恢复（通过 restore() 方法）。
+				this.ctx.arc(left + borderRadius, top + borderRadius, borderRadius, 0, 2 * Math.PI)
+				this.ctx.clip()
+				this.ctx.drawImage(value.url, left, top, width, height)
+			},
+			//画文字
+			canvasTextTap(){
+				// text	String	在画布上输出的文本
+				// x	Number	绘制文本的左上角x坐标位置
+				// y	Number	绘制文本的左上角y坐标位置
+				// maxWidth	Number	需要绘制的最大宽度，可选
+				const ctx = uni.createCanvasContext('myCanvas')
+				ctx.setFontSize(20)
+				ctx.setTextAlign('left')
+				ctx.fillText('你是谁啊', 180, 60)
+			
+				// 水平对齐 可选值 'top'、'bottom'、'middle'、'normal'
+				ctx.setTextBaseline('middle')
+			
+				ctx.draw()
+			},
+			opTap(){
+				const ctx = uni.createCanvasContext('myCanvas')
+				//设置绘图上下文的填充色为红色：
+				ctx.setFillStyle('red')
+				//用 fillRect(x, y, width, height) 方法画一个矩形，填充为刚刚设置的红色：
+				ctx.fillRect(10, 10, 150, 75)
+				ctx.setGlobalAlpha(0.2)
+				//设置绘图上下文的填充色为红色：
+				ctx.setFillStyle('blue')
+				//用 fillRect(x, y, width, height) 方法画一个矩形，填充为刚刚设置的红色：
+				ctx.fillRect(50, 50, 150, 75)
+				ctx.draw()
+			}	,
+			addImg(){
+				let that=this
+				uni.canvasToTempFilePath({
+				x: 0,
+				y: 0,
+				width: 300,
+				height: 300,
+				canvasId: 'myCanvas',
+				success: function(res) {
+				console.log(res)
+				that.imgURL=res.tempFilePath
+				// #ifdef H5
+				return '';
+				// #endif
+				that.bcImg(res.tempFilePath);
+				} 
+				})
+			},
+			bcImg(url){
+				uni.saveImageToPhotosAlbum({
+				filePath:url,
+				success: function () {
+				console.log('save success');
+				}
+				});
+			},
+			_onLoad(){
+				 this.ctx = uni.createCanvasContext('myCanvas');
+			}
 		}
 	}
 </script>
 
-<style lang="less">
-
+<style scoped lang="less">
+.canvas{
+	width: 99vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	
+}
 </style>
--- pages/item11/item11.vue
+++ pages/item11/item11.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<canvasImg></canvasImg>
+		<canvasImg :imgValue='pinImg'></canvasImg>
 	</view>
 </template>
 
@@ -10,47 +10,49 @@
 		data() {
 			return {
 				canvasParemis:'',
-				imgUrl:'../../static/2.png'
+				imgUrl:'../../static/3.png'
 			};
 		},
 		components: {
 			canvasImg
 		},
-		methods:{
+		computed:{
 			// 拼接图片内容
 			pinImg(){
-			  var that=this       
 				var img={
-			      width: 375,
-			      height: 578,
-			      views: [
-			        {
+				    width: 370,
+				    height: 478,
+				    views: [
+				      {
 						//矩形
-			          type:'rect',background:'#fff',top:120,left: 0,width: 375,height: 375
-			        },
-			        {
-			          type: 'image',url: that.imgUrl,top: 150,
-			          left: 147.5,width: 80,height: 80
-			        },
-			       
-			        {
-			          type: 'text',content: '超级魂斗罗',fontSize: 20,color: '#000',textAlign: 'center',
-			          top: 240, left: 187.5, width: 375,bolder: false
-			        },
-			       
-			        {
+				        type:'rect',background:'#fff',x:0,y: 0,width: 370,height: 478
+				      },
+				      {
+				        type: 'image',url: this.imgUrl,x: 150,
+				        y: 147.5,width: 80,height: 80
+				      },
+				     
+				      {
+				        type: 'text',content: '超级魂斗罗',fontSize: 20,color: '#000',textAlign: 'center',
+				        top: 240, left: 187.5, width: 375,bolder: false
+				      },
+				     
+				      {
 						//圆形图片
-			          type: 'Radius',url:  that.imgUrl,
-			          top: 300,left: 117.5, width: 70, height: 70,borderRadius:70
-			        },
-			        {
-			       	  type: 'text',content: '长按识别二维码进入小游戏~',fontSize: 14,color: '#c0bab9',textAlign: 'center',top: 450,
-			          left: 187.5, width: 375, lineHeight: 20,MaxLineNumber: 2,breakWord: true
-			        }        
-			      ]
-			  }
-			  return img
+				        type: 'radius',url:  this.imgUrl,
+				        x: 100,y: 117.5, width: 70, height: 70,borderRadius:70
+				      },
+				      {
+				     	  type: 'text',content: '长按识别二维码进入小游戏~',fontSize: 14,color: '#c0bab9',textAlign: 'center',top: 450,
+				        left: 187.5, width: 375, lineHeight: 20,MaxLineNumber: 2,breakWord: true
+				      }        
+				    ]
+				}
+				return img
 			}
+		},
+		methods:{
+			
 		}
 	}
 </script>
