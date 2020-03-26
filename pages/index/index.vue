01919e9c1e3cc40a67f2b0137b652cf5502c2127
lzj <1104792149@qq.com>
2020-03-26 17:32:30 +0800

    11

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,49 +1,68 @@
 <style>
-	button {
-		margin-top: 30upx;
-		background-color: red;
-		color: #fff;
-	}
+button {
+	margin-top: 30upx;
+	background-color: red;
+	color: #fff;
+}
 </style>
 <template>
 	<view>
-		<button @click="tap(1,'抽屉')">抽屉效果</button>
-		<button @click="tap(2,'上下弹窗')">上下弹窗效果</button>
-		<button @click="tap(3,'缩放弹窗')">缩放弹窗效果</button>
-		<button @click="tap(4,'公告')">公告效果</button>
-		<button @click="tap(5,'左右滚动')">左右滚动效果</button>
-		<button @click="tap(6,'转圈上下浮动')">转圈 上下浮动  效果</button>
-		<button @click="tap(7,'抽奖转盘')">抽奖转盘</button>
-		<button @click="tap(8,'红包雨')">红包雨</button>
-		<button @click="tap(9,'3d相册')">3d相册</button>
-		<button @click="tap(10,'canvas绘画')">canvas绘画</button>
-		<button @click="tap(11,'canvas绘制海报')">canvas绘制海报</button>
-		<button @click="tap(12,'登录授权')">登录授权</button>
-		<button @click="tap(13,'滑动抽屉')">滑动抽屉</button>
-		<button @click="tap(14,'测试滑动抽屉')">测试滑动抽屉</button>
-		<button @click="tap(15,'滑动导航栏')">滑动导航栏</button>
-		<button @click="tap(16,'图片懒加载')">图片懒加载</button>
-		<button @click="tap(17,'加强型滑动导航栏')">加强型滑动导航栏</button>
-		<button @click="tap(18,'数字签名')">数字签名</button>
+		<button @click="tap(1, '抽屉')">抽屉效果</button>
+		<button @click="tap(2, '上下弹窗')">上下弹窗效果</button>
+		<button @click="tap(3, '缩放弹窗')">缩放弹窗效果</button>
+		<button @click="tap(4, '公告')">公告效果</button>
+		<button @click="tap(5, '左右滚动')">左右滚动效果</button>
+		<button @click="tap(6, '转圈上下浮动')">转圈 上下浮动 效果</button>
+		<button @click="tap(7, '抽奖转盘')">抽奖转盘</button>
+		<button @click="tap(8, '红包雨')">红包雨</button>
+		<button @click="tap(9, '3d相册')">3d相册</button>
+		<button @click="tap(10, 'canvas绘画')">canvas绘画</button>
+		<button @click="tap(11, 'canvas绘制海报')">canvas绘制海报</button>
+		<button @click="tap(12, '登录授权')">登录授权</button>
+		<button @click="tap(13, '滑动抽屉')">滑动抽屉</button>
+		<button @click="tap(14, '测试滑动抽屉')">测试滑动抽屉</button>
+		<button @click="tap(15, '滑动导航栏')">滑动导航栏</button>
+		<button @click="tap(16, '图片懒加载')">图片懒加载</button>
+		<button @click="tap(17, '加强型滑动导航栏')">加强型滑动导航栏</button>
+		<button @click="tap(18, '数字签名')">数字签名</button>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {
+		var value = this.toChinese(1030);
+		console.info(value);
+	},
+	methods: {
+		tap(type, title) {
+			uni.navigateTo({
+				url: `../item${type}/item${type}?title=${title}+item${type}`
+			});
+		},
+
+		// 精度只限于正整数（后期再优化）
+		toChinese: function(num) {
+			var num = parseInt(num);
+			var overWan = Math.floor(num / 10000);
+			var noWan = num % 10000;
+			if (noWan.toString().length < 4) noWan = '0' + noWan;
+			return overWan ? this.getWan(overWan) + '万' + this.getWan(noWan) : this.getWan(num);
+		},
+		getWan:function(temp){
+			var changeNum = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']; //changeNum[0] = "零"
+			var unit = ['', '十', '百', '千', '万'];
+			var strArr = temp.toString().split('').reverse();
+			var newNum = '';
+			for (var i = 0; i < strArr.length; i++) {
+				newNum =(i == 0 && strArr[i] == 0 ? '' : i > 0 && strArr[i] == 0 && strArr[i - 1] == 0 ? '' : changeNum[strArr[i]] + (strArr[i] == 0 ? unit[0] : unit[i])) + newNum;
 			}
-		},
-		onLoad() {
-		},
-		methods: {
-			tap(type,title) {
-				uni.navigateTo({
-					url: `../item${type}/item${type}?title=${title}+item${type}`
-				})
-			}
-		},
-		watch:{
+			return newNum;
 		}
-	}
+	},
+	watch: {}
+};
 </script>
--- pages/item1/item1.vue
+++ pages/item1/item1.vue
@@ -1,51 +1,98 @@
 <style scoped lang="less">
-.item1-000{
+@-webkit-keyframes fadeIn {
+	0% {
+		transform: translateX(100%);
+	}
+	100% {
+		transform: translateX(0%);
+	}
+}
+@-webkit-keyframes fadeIn2 {
+	0% {
+		transform: translateX(0%);
+	}
+	100% {
+		transform: translateX(100%);
+	}
+}
+.item1-000 {
 	width: 100vw;
 	height: 100vh;
-	background:rgba(34,34,34, 0.4);
+	background: rgba(34, 34, 34, 0.4);
 	position: fixed;
-	top: 0;
 	left: 0;
+	top: 0;
+	transition: all 0.3s linear;
 	transform: translateX(100%);
-	transition: all 0.7s;
-	&.show {
-	    transform: translateX(0);
+	&.show{
+		transform: translateX(0%);
 	}
-	
 }
-.item1-fff{
+
+.item1-title {
+	height: 280upx;
+	width: 100%;
+	background: red;
+	position: absolute;
+	top :0;
+	left: 0;
+	z-index: 2;
+}
+.item1-value{
 	height: 100%;
-	width: 80%;
-	margin-left: 20%;
-	background: cadetblue;
+	width: 100%;
+	background: blueviolet;
+	position: absolute;
+	top :0;
+	left: 0;
+	z-index: 1;
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 500upx;
+}
+.dh{
+	-webkit-animation-name: fadeIn; /*动画名称*/
+	-webkit-animation-duration: 0.3s; /*动画持续时间*/
+	-webkit-animation-iteration-count: 1; /*动画次数*/
+	-webkit-animation-delay: 0s; /*延迟时间*/
+	-webkit-animation-timing-function: linear;
 }
 </style>
 
 <template>
 	<view>
-		<view class="item1-000" :class="{show:status}">
-			<view class="item1-fff"></view>
+		<button @click="status1=!status1">抽屉效果</button>
+		<view  class="item1-000" :class="{show:status1}" >
+			<view class="item1-title"  @click.stop="status1=!status1"></view>
+			<view @click="ctTap2" class="item1-value" :class="[index2>0?'dh':'']" :key="index2" v-for="(itme2, index2) in number2">
+				{{index2}}
+			</view>
 		</view>
-		<button @click="ctTap">抽屉效果</button>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				status:false
-			};
+export default {
+	data() {
+		return {
+			status: false,
+			number1: 0,
+			status1: false,
+			number2: 1
+		};
+	},
+	methods: {
+		ctTap2() {
+			this.number2++;
+			console.log(this.number2);
 		},
-		methods:{
-			ctTap(){
-				this.status=!this.status;
-			}
-		},
-		onLoad(e) {
-			uni.setNavigationBarTitle({
-				title: e.title
-			});}
+	},
+	onLoad(e) {
+		uni.setNavigationBarTitle({
+			title: e.title
+		});
 	}
+};
 </script>
-
